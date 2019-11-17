31772e7062a6eefce718ec5fe88f81bc883c9e4c
Горшенев Леонид <leon.leonc@yandex.ru>
2019-11-17 11:45:32 +0300

    Tank class added!

--- main.py
+++ main.py
@@ -5,23 +5,91 @@ size = width, height = 800, 600
 black = 0, 0, 0
 
 
+class Tank():
+    _tank_u = pygame.image.load("./Sprites/tank_u.png")
+    _tank_d = pygame.image.load("./Sprites/tank_d.png")
+    _tank_r = pygame.image.load("./Sprites/tank_r.png")
+    _tank_l = pygame.image.load("./Sprites/tank_l.png")
 
-def main():
+    _move_right = False
+    _move_left = False
+    _move_up = False
+    _move_down = False
+    current_shift = 0
 
-    tank_u = pygame.image.load("tank_u.png")
-    tank_d = pygame.image.load("tank_d.png")
-    tank_r = pygame.image.load("tank_r.png")
-    tank_l = pygame.image.load("tank_l.png")
-
-    tankrect = tank_u.get_rect()
+    tankrect = _tank_u.get_rect()
     tankrect.x = 100
     tankrect.y = 100
 
-    move_right = False
-    move_left = False
-    move_up = False
-    move_down = False
-    current_shift = 0
+    tank_pic = _tank_r
+
+    def __init__(self):
+        pass
+
+    def keyEventDown(self, event):
+        if chr(event.key) == 'd':
+            self._move_right = True
+            self.current_shift = 1
+        elif chr(event.key) == 'a':
+            self._move_left = True
+            self.current_shift = -1
+        elif chr(event.key) == 'w':
+            self._move_up = True
+            self.current_shift = 1
+        elif chr(event.key) == 's':
+            self._move_down = True
+            self.current_shift = -1
+
+    def keyEventUp(self, event):
+        if chr(event.key) == 'd':
+            self._move_right = False
+            self.current_shift = 0
+        if chr(event.key) == 'a':
+            self._move_left = False
+            self.current_shift = 0
+        if chr(event.key) == 'w':
+            self._move_up = False
+            self.current_shift = 0
+        if chr(event.key) == 's':
+            self._move_down = False
+            self.current_shift = 0
+
+    def draw(self, screen):
+        screen.blit(self.tank_pic, self.tankrect)
+
+    def move(self):
+        if self._move_right:
+            self.tank_pic = self._tank_r
+            self.tankrect.x += 2
+            self.tankrect.x += self.current_shift
+        if self._move_left:
+            self.tank_pic = self._tank_l
+            self.tankrect.x -= 2
+            self.tankrect.x += self.current_shift
+        if self._move_up:
+            self.tank_pic = self._tank_u
+            self.tankrect.y -= 2
+            self.tankrect.y += self.current_shift
+
+        if self._move_down:
+            self.tank_pic = self._tank_d
+            self.tankrect.y += 2
+            self.tankrect.y += self.current_shift
+        self._check_collisions()
+
+    def _check_collisions(self):
+        if self.tankrect.x > width - 40:
+            self.tankrect.x = width - 40
+        if self.tankrect.y > height - 40:
+            self.tankrect.y = height - 40
+        if self.tankrect.x < 0:
+            self.tankrect.x = 0
+        if self.tankrect.y < 0:
+            self.tankrect.y = 0
+
+
+def game_test_loop():
+    tank = Tank()
 
     pygame.init()
     screen = pygame.display.set_mode(size)
@@ -31,68 +99,21 @@ def main():
             if event.type == pygame.QUIT:
                 game_over = True
             elif event.type == pygame.KEYDOWN:
-                if chr(event.key) == 'd':
-                    move_right = True
-                    current_shift = 1
-                if chr(event.key) == 'a':
-                    move_left = True
-                    current_shift = -1
-                if chr(event.key) == 'w':
-                    move_up = True
-                    current_shift = 1
-                if chr(event.key) == 's':
-                    move_down = True
-                    current_shift = -1
+                tank.keyEventDown(event)
             elif event.type == pygame.KEYUP:
-                if chr(event.key) == 'd':
-                    move_right = False
-                    current_shift = 0
-                if chr(event.key) == 'a':
-                    move_left = False
-                    current_shift = 0
-                if chr(event.key) == 'w':
-                    move_up = False
-                    current_shift = 0
-                if chr(event.key) == 's':
-                    move_down = False
-                    current_shift = 0
+                tank.keyEventUp(event)
 
         screen.fill(black)
 
         # Код отрисовки пишется здесь
-        screen.blit(tank_r, tankrect)
+        tank.draw(screen)
 
-        if move_right:
-            screen.blit(tank_r, tankrect)
-            tankrect.x += 2
-            tankrect.x += current_shift
-        if move_left:
-            screen.blit(tank_l, tankrect)
-            tankrect.x -= 2
-            tankrect.x += current_shift
-        if move_up:
-            screen.blit(tank_u, tankrect)
-            tankrect.y -= 2
-            tankrect.y += current_shift
-
-        if move_down:
-            screen.blit(tank_d, tankrect)
-            tankrect.y += 2
-            tankrect.y += current_shift
-
-        if tankrect.x > width - 40:
-            tankrect.x = width - 40
-        if tankrect.y > height - 40:
-            tankrect.y = height - 40
-        if tankrect.x < 0:
-            tankrect.x = 0
-        if tankrect.y < 0:
-            tankrect.y = 0
-
+        tank.move()
 
         pygame.display.flip()
         pygame.time.wait(10)
     sys.exit()
 
+
 if __name__ == '__main__':
-    main()
+    game_test_loop()
