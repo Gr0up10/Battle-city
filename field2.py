50aac065d2a6c6a8839467f9b6cd1b11da2b39eb
Дутчак Денис <qwop507@yandex.ru>
2019-11-23 14:51:32 +0000

    Added group of destructible blocks

--- field2.py
+++ field2.py
@@ -7,6 +7,9 @@ block_size = 50
 
 
 class field2:
+    #группа разрушаемых блоков
+    bricks = pygame.sprite.Group()
+    
     def __init__(self, level_path='level_1.txt'):
         self.blocks = list()
         self.level_path = level_path
@@ -26,6 +29,8 @@ class field2:
             for char in string:
                 if char != 'd':
                     img_path = 'sprites/blocks/' + char + '.png'
+                    if char == '0' or char == '1' or char == '2' or char == '3' or char == '4':
+                        self.bricks.add(Block2(img_path, x * block_size, y * block_size))
                     self.blocks.append(Block2(img_path, x * block_size, y * block_size))
                 x += 1
             y += 1
