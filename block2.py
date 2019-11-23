07c445629182d60ea9b7f9e79a442cbe37a2f3b0
Горшенев Леонид <leon.leonc@yandex.ru>
2019-11-23 13:50:03 +0300

    Textures for field are drawing now

--- block2.py
+++ block2.py
@@ -7,9 +7,12 @@ class Block2(pygame.sprite.Sprite):
     def __init__(self, image_path, x, y,is_wall=False):
         pygame.sprite.Sprite.__init__(self)
         self.image_path = image_path
-        #self.image = pygame.image.load(image_path)           # раскомментировать, когда нужно будет накладывать текстуры
-        self.image = pygame.Surface((block_size, block_size)) #убрать, когда нужно будет накладывать текстуры
-        self.image.fill((255,0,0))                            # убрать, когда нужно будет накладывать текстуры
+        if image_path[15] in ('4','9','a','b','c'):
+            self.image = pygame.transform.scale(pygame.image.load(image_path), (50, 50))
+        else:
+            self.image = pygame.transform.scale(pygame.image.load(image_path), (50, 25))
+        #self.image = pygame.Surface((block_size, block_size)) #убрать, когда нужно будет накладывать текстуры
+        #self.image.fill((255,0,0))                            # убрать, когда нужно будет накладывать текстуры
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
