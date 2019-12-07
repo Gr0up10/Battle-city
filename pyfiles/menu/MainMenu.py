794081af8188a26aa6403e130c3e7a163b987150
Горшенев Леонид <leon.leonc@yandex.ru>
2019-12-07 14:50:18 +0300

    Menu show for windows template

--- pyfiles/menu/MainMenu.py
+++ pyfiles/menu/MainMenu.py
@@ -21,73 +21,79 @@ class MainMenu:
         self.screen = pygame.display.set_mode(self.size)
 
     def show(self):
-        pygame.init()
-        pygame.font.init()
+        if sys.platform == 'linux':
+            pygame.init()
+            pygame.font.init()
 
-        screen = pygame.display.set_mode(self.size)
-        # текст на кнопках
-        font = pygame.font.SysFont('Comic Sans MS', 45, True)
-        data1 = '1 player'
-        data2 = '2 players'
-        data3 = 'custom map'
-        data0 = 'Есть пробитие!'
-        # --------------
-        game_over = False
+            screen = pygame.display.set_mode(self.size)
+            # текст на кнопках
+            font = pygame.font.SysFont('Comic Sans MS', 45, True)
+            data1 = '1 player'
+            data2 = '2 players'
+            data3 = 'custom map'
+            data0 = 'Есть пробитие!'
+            # --------------
+            game_over = False
 
-        while not game_over:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    game_over = True
+            while not game_over:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        game_over = True
 
-                elif event.type == pygame.KEYDOWN:
+                    elif event.type == pygame.KEYDOWN:
 
-                    if chr(event.key) == 'w' or event.key == pygame.K_UP:
-                        self.integ -= 1
-                    elif chr(event.key) == 's' or event.key == pygame.K_DOWN:
-                        self.integ += 1
+                        if chr(event.key) == 'w' or event.key == pygame.K_UP:
+                            self.integ -= 1
+                        elif chr(event.key) == 's' or event.key == pygame.K_DOWN:
+                            self.integ += 1
 
-                    if self.integ > 1:      # заменить 1 на 2, когда будем делать редактор карт !!!!!!!!!!
-                        self.integ = 0
-                    elif self.integ < 0:
-                        self.integ = 1       # заменить 1 на 2, когда будем делать редактор карт !!!!!!!!!
-                    if self.integ == 0:
-                        self.tank_rect.y = 155
-                    elif self.integ == 1:
-                        self.tank_rect.y = 305
-                    elif self.integ == 2:
-                        self.tank_rect.y = 455
-
-                    if event.key == 32 or event.key == pygame.K_RETURN:
+                        if self.integ > 1:      # заменить 1 на 2, когда будем делать редактор карт !!!!!!!!!!
+                            self.integ = 0
+                        elif self.integ < 0:
+                            self.integ = 1       # заменить 1 на 2, когда будем делать редактор карт !!!!!!!!!
                         if self.integ == 0:
-                            return 1
-                        if self.integ == 1:
-                            return 2
-                    # блок для функций
-            # if integ == 0 and chr(event.key) == 'space' or chr(event.key) == 'enter':
-            #      onePlayerGame() # 1 игрок
-            #   elif integ == 1 and chr(event.key) == 'space' or chr(event.key) == 'enter':
-            #        twoPlayerGame() # 2 игрока
-            #     elif integ == 2 and chr(event.key) == 'space' or chr(event.key) == 'enter':
-            #          createCustomMap() # кастомную карту
+                            self.tank_rect.y = 155
+                        elif self.integ == 1:
+                            self.tank_rect.y = 305
+                        elif self.integ == 2:
+                            self.tank_rect.y = 455
 
-            screen.fill(self.colors["black"])
+                        if event.key == 32 or event.key == pygame.K_RETURN:
+                            if self.integ == 0:
+                                return 1
+                            if self.integ == 1:
+                                return 2
+                        # блок для функций
+                # if integ == 0 and chr(event.key) == 'space' or chr(event.key) == 'enter':
+                #      onePlayerGame() # 1 игрок
+                #   elif integ == 1 and chr(event.key) == 'space' or chr(event.key) == 'enter':
+                #        twoPlayerGame() # 2 игрока
+                #     elif integ == 2 and chr(event.key) == 'space' or chr(event.key) == 'enter':
+                #          createCustomMap() # кастомную карту
 
-            ts1 = font.render(data1, False, self.colors["white"])
-            screen.blit(ts1, (300, self.ra + 10))
-            ts2 = font.render(data2, False, self.colors["white"])
-            screen.blit(ts2, (300, self.ra * 2 + 10))
-            #ts3 = font.render(data3, False, self.colors["white"])          # отрисовка тертьего текста
-            #screen.blit(ts3, (300, self.ra * 3 + 10))
+                screen.fill(self.colors["black"])
 
-            ts0 = font.render(data0, False, self.colors["white"])  # главный текст пробития
-            screen.blit(ts0, (280, 50))
+                ts1 = font.render(data1, False, self.colors["white"])
+                screen.blit(ts1, (300, self.ra + 10))
+                ts2 = font.render(data2, False, self.colors["white"])
+                screen.blit(ts2, (300, self.ra * 2 + 10))
+                #ts3 = font.render(data3, False, self.colors["white"])          # отрисовка тертьего текста
+                #screen.blit(ts3, (300, self.ra * 3 + 10))
 
-            screen.blit(self.tank_imag, self.tank_rect)
+                ts0 = font.render(data0, False, self.colors["white"])  # главный текст пробития
+                screen.blit(ts0, (280, 50))
 
-            for i in range(2):                              # заменить 2 на 3, когда будем делать редктор карт
-                pygame.draw.rect(screen, self.colors["white"], (200, (i + 1) * self.ra, 400, 50), 2)
+                screen.blit(self.tank_imag, self.tank_rect)
 
-            pygame.display.flip()
-            pygame.time.wait(10)
+                for i in range(2):                              # заменить 2 на 3, когда будем делать редктор карт
+                    pygame.draw.rect(screen, self.colors["white"], (200, (i + 1) * self.ra, 400, 50), 2)
 
-        sys.exit(0)
+                pygame.display.flip()
+                pygame.time.wait(10)
+
+            sys.exit(0)
+        else:
+            self.show_windows()
+
+    def show_windows(self):
+        pass
